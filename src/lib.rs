fba21fd3592fd99b9edb1a70e8a2cea3cd18a522
doomy <2640792-_doomy@users.noreply.gitlab.com>
2021-09-06 18:52:13 -0500

    xD

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,7 @@ impl Plugin for PuzzlePlugin {
         app.add_state(GameState::Load)
             .init_resource::<Score>()
             .init_resource::<ActiveEntity>()
-            .init_resource::<BlockResource<states::Empty>>()
-            .init_resource::<BlockResource<states::Hover>>()
-            .init_resource::<BlockResource<states::Full>>()
-            .init_resource::<BlockResource<states::Invalid>>()
-            .init_resource::<BlockResource<states::Scored>>()
+            .init_resource::<BlockResources>()
             .init_resource::<Handle<SettingsAsset>>()
             .init_resource::<CursorPosition>()
             .add_asset::<Pattern>()
@@ -44,9 +40,9 @@ impl Plugin for PuzzlePlugin {
                 SystemSet::on_update(GameState::Main)
                     .with_system(input_system.system())
                     .with_system(add_sprite_to_tiles.system())
-                    .with_system(highlight_under_cursor.system())
                     .with_system(active_follow_mouse.system())
                     .with_system(commit_on_click.system())
+                    .with_system(update_hovered_board_pieces.system())
                     .label("main"),
             )
             .add_system_set(
@@ -58,11 +54,7 @@ impl Plugin for PuzzlePlugin {
             )
             .add_system_set(
                 SystemSet::on_update(GameState::Main)
-                    .with_system(style::<states::Empty>.system())
-                    .with_system(style::<states::Hover>.system())
-                    .with_system(style::<states::Full>.system())
-                    .with_system(style::<states::Invalid>.system())
-                    .with_system(style::<states::Scored>.system())
+                    .with_system(style_blocks.system())
                     .label("styles")
                     .after("main"),
             );
@@ -80,11 +72,7 @@ fn load_setup(
     mut loading: ResMut<PreloadingAssets>,
     mut settings_handle: ResMut<Handle<SettingsAsset>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut empty: ResMut<BlockResource<states::Empty>>,
-    mut hover: ResMut<BlockResource<states::Hover>>,
-    mut full: ResMut<BlockResource<states::Full>>,
-    mut invalid: ResMut<BlockResource<states::Invalid>>,
-    mut scored: ResMut<BlockResource<states::Scored>>,
+    mut style: ResMut<BlockResources>,
 ) {
     // load our settings file
     *settings_handle = assets.load("settings.rfg");
@@ -101,12 +89,13 @@ fn load_setup(
         (texture.clone(), material.clone())
     };
 
-    // Load all blocks
-    *empty = BlockResource::<states::Empty>::new(load_tex("empty"));
-    *hover = BlockResource::<states::Hover>::new(load_tex("hover"));
-    *full = BlockResource::<states::Full>::new(load_tex("full"));
-    *invalid = BlockResource::<states::Invalid>::new(load_tex("invalid"));
-    *scored = BlockResource::<states::Scored>::new(load_tex("scored"));
+    *style = BlockResources {
+        empty: BlockResource::new(load_tex("empty")),
+        full: BlockResource::new(load_tex("full")),
+        hover: BlockResource::new(load_tex("hover")),
+        invalid: BlockResource::new(load_tex("invalid")),
+        scored: BlockResource::new(load_tex("scored")),
+    };
 
     assets
         .watch_for_changes()
@@ -149,6 +138,7 @@ fn game_setup(
                 Transform::from_xyz(x as f32, y as f32, 0f32),
                 GameBoard,
                 Tile,
+                selection::None,
             ));
         }
     }
@@ -294,34 +284,27 @@ impl Pattern {
 }
 
 #[derive(Default, Clone)]
-pub struct BlockResource<T> {
-    phantom: PhantomData<T>,
+pub struct BlockResources {
+    pub empty: BlockResource,
+    pub full: BlockResource,
+    pub hover: BlockResource,
+    pub invalid: BlockResource,
+    pub scored: BlockResource,
+}
+
+#[derive(Default, Clone)]
+pub struct BlockResource {
     pub texture: Handle<Texture>,
     pub material: Handle<ColorMaterial>,
 }
 
-impl<T> BlockResource<T> {
+impl BlockResource {
     pub fn new(tex_mat: (Handle<Texture>, Handle<ColorMaterial>)) -> Self {
         Self {
             texture: tex_mat.0.clone(),
             material: tex_mat.1.clone(),
-            phantom: PhantomData,
         }
     }
-}
-
-#[derive(Default)]
-pub struct Textures {
-    pub empty: Handle<Texture>,
-    pub hover: Handle<Texture>,
-    pub full: Handle<Texture>,
-}
-
-#[derive(Default)]
-pub struct Materials {
-    pub empty: Handle<ColorMaterial>,
-    pub hover: Handle<ColorMaterial>,
-    pub full: Handle<ColorMaterial>,
 }
 
 pub fn add_sprite_to_tiles(mut cmd: Commands, query: Query<(Entity, &Transform), Added<Tile>>) {
@@ -333,29 +316,6 @@ pub fn add_sprite_to_tiles(mut cmd: Commands, query: Query<(Entity, &Transform),
             global_transform: transform.clone().into(),
             ..Default::default()
         });
-    });
-}
-
-pub fn style<T: Component>(
-    mut cmd: Commands,
-    query: Query<(Entity, Option<&Color>), Added<T>>,
-    style: Res<BlockResource<T>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    query.for_each_mut(|(entity, color)| {
-        let texture = materials
-            .get(style.material.clone())
-            .unwrap()
-            .texture
-            .as_ref()
-            .unwrap()
-            .clone();
-        // TODO: lol
-        let new = materials.add(ColorMaterial {
-            color: *color.unwrap_or(&Color::WHITE),
-            texture: Some(texture),
-        });
-        cmd.entity(entity).insert(new.clone());
     });
 }
 
@@ -380,82 +340,165 @@ pub fn pattern_builder<T: Component + Default>(
         .id()
 }
 
-pub fn highlight_under_cursor(
+pub type ActiveCoordinates = Vec<Vec2>;
+
+pub fn update_hovered_board_pieces(
     mut cmd: Commands,
-    empty: Query<(Entity, &Transform), (With<states::Empty>, With<GameBoard>)>,
-    hover: Query<(Entity, &Transform), (With<states::Hover>, With<GameBoard>)>,
-    full: Query<(Entity, &Transform), (With<states::Full>, With<GameBoard>)>,
-    invalid: Query<(Entity, &Transform), (With<states::Invalid>, With<GameBoard>)>,
     active: Query<Entity, With<ActiveEntity>>,
     children: Query<&Children>,
     transforms: Query<&GlobalTransform>,
+    blank_tiles: Query<
+        (Entity, &Transform),
+        (
+            With<states::Empty>,
+            Without<selection::Hover>,
+            With<GameBoard>,
+        ),
+    >,
+    hovered_blank_tiles: Query<
+        (Entity, &Transform),
+        (With<states::Empty>, With<selection::Hover>, With<GameBoard>),
+    >,
+    full_tiles: Query<
+        (Entity, &Transform),
+        (With<states::Full>, With<selection::None>, With<GameBoard>),
+    >,
+    invalid_full_tiles: Query<
+        (Entity, &Transform),
+        (
+            With<states::Full>,
+            With<selection::Invalid>,
+            With<GameBoard>,
+        ),
+    >,
 ) {
-    // get all blocks in the active pattern
-    active.for_each(|entity| {
-        // compare and highlight tiles on the gameboard
+    // Our active entity contains children of the actual tiles which we get here
+    active
+        .single()
+        .map(|entity| {
+            // get all blocks in the active pattern
+            // compare and highlight tiles on the gameboard
+            let coords: Vec<Vec2> = children
+                .get(entity)
+                .unwrap()
+                .iter()
+                .filter_map(|active_entity| match transforms.get(*active_entity) {
+                    Ok(transform) => Some(transform.board_position()),
+                    Err(_) => None,
+                })
+                .collect();
 
-        // get all active piece coordinates and collect into a vec
-        let active_coordinates: Vec<Vec2> = children
-            .get(entity)
-            .unwrap()
-            .iter()
-            .filter_map(|active_entity| match transforms.get(*active_entity) {
-                Ok(transform) => Some(Vec2::new(
-                    transform.translation.x.round(),
-                    transform.translation.y.round(),
-                )),
-                Err(_) => None,
-            })
-            .collect();
+            // add hover to blank tiles that match the active piece transform
+            blank_tiles.for_each(|(e, t)| {
+                if coords.contains(&t.board_position()) {
+                    transition::<selection::None, selection::Hover>(&mut cmd, e);
+                }
+            });
 
-        let vec_floor = |t: &Transform| Vec2::new(t.translation.x.floor(), t.translation.y.floor());
+            // remove hover if coords no longer contains
+            hovered_blank_tiles.for_each(|(e, t)| {
+                if !coords.contains(&t.board_position()) {
+                    transition::<selection::Hover, selection::None>(&mut cmd, e);
+                }
+            });
 
-        // Change to hovering for empty
-        empty.for_each_mut(|(entity, transform)| {
-            let pos = vec_floor(transform);
-            if active_coordinates.contains(&pos) {
-                // add hover marker and remove empty
-                cmd.entity(entity)
-                    .remove::<states::Empty>()
-                    .insert(states::Hover);
-            }
+            // add invalid to full hovers
+            full_tiles.for_each(|(e, t)| {
+                if coords.contains(&t.board_position()) {
+                    transition::<selection::None, selection::Invalid>(&mut cmd, e);
+                }
+            });
+
+            // removes invalid from full no longer hovered
+            invalid_full_tiles.for_each(|(e, t)| {
+                if !coords.contains(&t.board_position()) {
+                    transition::<selection::Invalid, selection::None>(&mut cmd, e);
+                }
+            });
+        })
+        .ok();
+}
+
+fn style_blocks(
+    mut cmd: Commands,
+    styles: Res<BlockResources>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    full: Query<(Entity, Option<&Color>), Added<states::Full>>,
+    empty: Query<Entity, (Added<states::Empty>, With<selection::None>)>,
+    scored: Query<Entity, (Added<states::Scored>, With<selection::None>)>,
+    invalid: Query<Entity, (With<states::Full>, Added<selection::Invalid>)>,
+    hovered: Query<Entity, (With<states::Empty>, Added<selection::Hover>)>,
+    unhovered: Query<Entity, (With<states::Empty>, Added<selection::None>)>,
+    uninvalidated: Query<(Entity, Option<&Color>), (With<states::Full>, Added<selection::None>)>,
+) {
+    full.iter()
+        .chain(uninvalidated.iter())
+        .for_each(|(entity, color)| {
+            style(
+                &mut cmd,
+                entity,
+                styles.full.texture.clone(),
+                color.cloned(),
+                &mut materials,
+            );
         });
-
-        // Invalid states do not remove any other states and preserve the underlying block type that is important to the game
-
-        // Add an invalid state to full blocks
-        full.for_each_mut(|(entity, transform)| {
-            let pos = vec_floor(transform);
-            if active_coordinates.contains(&pos) {
-                cmd.entity(entity).insert(states::Invalid);
-            }
-        });
-
-        // Remove invalid state when no longer hovered
-        invalid.for_each_mut(|(entity, transform)| {
-            let pos = vec_floor(transform);
-            if !active_coordinates.contains(&pos) {
-                cmd.entity(entity).remove::<states::Invalid>();
-            }
-        });
-
-        // Unhover blocks that aren't hovered anymore
-        hover.for_each_mut(|(entity, transform)| {
-            let pos = vec_floor(transform);
-            if !active_coordinates.contains(&pos) {
-                // remove hover marker and add empty
-                cmd.entity(entity)
-                    .remove::<states::Hover>()
-                    .insert(states::Empty);
-            }
-        });
+    empty.iter().chain(unhovered.iter()).for_each(|entity| {
+        style(
+            &mut cmd,
+            entity,
+            styles.empty.texture.clone(),
+            None,
+            &mut materials,
+        );
     });
+    scored.for_each(|entity| {
+        style(
+            &mut cmd,
+            entity,
+            styles.scored.texture.clone(),
+            None,
+            &mut materials,
+        );
+    });
+    invalid.for_each(|entity| {
+        style(
+            &mut cmd,
+            entity,
+            styles.invalid.texture.clone(),
+            None,
+            &mut materials,
+        );
+    });
+    hovered.for_each(|entity| {
+        style(
+            &mut cmd,
+            entity,
+            styles.hover.texture.clone(),
+            None,
+            &mut materials,
+        );
+    });
+}
+
+pub fn style(
+    cmd: &mut Commands,
+    entity: Entity,
+    texture: Handle<Texture>,
+    color: Option<Color>,
+    materials: &mut Assets<ColorMaterial>,
+) {
+    // TODO: lol
+    let new_material = materials.add(ColorMaterial {
+        color: color.unwrap_or(Color::WHITE),
+        texture: Some(texture),
+    });
+    cmd.entity(entity).insert(new_material.clone());
 }
 
 pub fn commit_on_click(
     mut cmd: Commands,
-    invalid: Query<&states::Invalid>,
-    hover: Query<Entity, (With<states::Hover>, With<GameBoard>)>,
+    invalid: Query<(), With<selection::Invalid>>,
+    hover: Query<Entity, (With<selection::Hover>, With<states::Empty>, With<GameBoard>)>,
     mouse: Res<Input<MouseButton>>,
     active: Query<Entity, With<ActiveEntity>>,
     colors: Query<&Color>,
@@ -467,13 +510,13 @@ pub fn commit_on_click(
         // first, ensure there are no invalid blocks
         if invalid.iter().count() == 0 {
             if let Some(active_entity) = active.iter().next() {
+                // also ensure that the number of hovered blocks matches the number of active
                 let active_blocks_count = children
                     .get(active_entity)
                     .map(|x| x.iter().count())
                     .unwrap_or(0);
-
-                // also ensure that the number of hovered blocks matches the number of active
                 if hover.iter().count() == active_blocks_count {
+                    // get block color to commit to the game board
                     let mut color = Color::WHITE;
                     if let Some(entity) = active.iter().next() {
                         if let Ok(children) = children.get(entity) {
@@ -485,11 +528,11 @@ pub fn commit_on_click(
                         };
                     }
 
+                    // commit
                     hover.for_each(|e| {
-                        cmd.entity(e)
-                            .remove::<states::Hover>()
-                            .insert(states::Full)
-                            .insert(color);
+                        transition::<states::Empty, states::Full>(&mut cmd, e);
+                        transition::<selection::Hover, selection::None>(&mut cmd, e);
+                        cmd.entity(e).insert(color);
                     });
 
                     // reset the piece
@@ -542,9 +585,7 @@ pub fn scorer(
     });
 
     for e in scoring_tiles {
-        cmd.entity(e)
-            .remove::<states::Full>()
-            .insert(states::Scored);
+        transition::<states::Full, states::Scored>(&mut cmd, e);
     }
 }
 
@@ -555,10 +596,13 @@ pub fn score_scored(
 ) {
     scored.for_each(|e| {
         *score += 1;
+        cmd.entity(e).remove::<Color>();
+        transition::<states::Scored, states::Empty>(&mut cmd, e);
+        // manually reset selection state back to none no matter what state we are currently
         cmd.entity(e)
-            .remove::<states::Scored>()
-            .remove::<Color>()
-            .insert(states::Empty);
+            .remove::<selection::Invalid>()
+            .remove::<selection::Hover>()
+            .insert(selection::None);
     });
 }
 
@@ -568,23 +612,56 @@ pub struct GameBoard;
 /// Includes every tile ever
 pub struct Tile;
 
+/// Only one of the components here should be used
 pub mod states {
-    pub fn all() -> Vec<&'static str> {
-        vec!["empty", "full", "hover", "ghost", "invalid", "scored"]
-    }
+    use bevy::{ecs::component::Component, prelude::*};
+
     #[derive(Default)]
     pub struct Empty;
     #[derive(Default)]
     pub struct Full;
-    #[derive(Default)]
-    pub struct Hover;
-    #[derive(Default)]
-    pub struct Ghost;
-    #[derive(Default)]
-    pub struct Invalid;
     /// Already scored tile that will be cleaned up
     #[derive(Default)]
     pub struct Scored;
 }
 
+pub mod selection {
+    #[derive(Default)]
+    pub struct None;
+    #[derive(Default)]
+    pub struct Hover;
+
+    #[derive(Default)]
+    pub struct Invalid;
+}
+
+/// Transition states in a fn as to avoid invalid states
+#[inline(always)]
+pub fn transition<F, T>(cmd: &mut Commands, entity: Entity)
+where
+    F: Component,
+    T: Component + Default,
+{
+    cmd.entity(entity).remove::<F>().insert(T::default());
+}
+
 struct ScoreText;
+
+trait TransformExt {
+    /// Returns a two-dimentional rounded coordinates useful for comparing game pieces  
+    fn board_position(&self) -> Vec2;
+}
+
+impl TransformExt for Transform {
+    fn board_position(&self) -> Vec2 {
+        let t = self.translation;
+        Vec2::new(t.x.round(), t.y.round())
+    }
+}
+
+impl TransformExt for GlobalTransform {
+    fn board_position(&self) -> Vec2 {
+        let t = self.translation;
+        Vec2::new(t.x.round(), t.y.round())
+    }
+}
